ea80230744dd9fd3ee5a90b10eb743d6eab1a285
gallifrey <956465331@qq.com>
2023-04-07 11:05:03 +0800

    feat: add MaxAllowed Middleware and write unit test
    - add a middleware function set a size limit on the uploaded files
    - add an uint test to test MaxAllowed Middleware
    - add a "todo" to extract code to generate files

--- router/router.go
+++ router/router.go
@@ -3,11 +3,33 @@ package router
 import (
 	"github.com/gallifreyCar/gin-file-server/handler"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
+
+// MaxAllowed Middleware function to set a size limit on the uploaded files
+func MaxAllowed(n int64) gin.HandlerFunc {
+	maxBytes := n
+	return func(c *gin.Context) {
+		c.Request.Body = http.MaxBytesReader(c.Writer, c.Request.Body, maxBytes)
+		if err := c.Request.ParseMultipartForm(maxBytes); err != nil {
+			if err.Error() == "http: request body too large" {
+				c.AbortWithStatus(http.StatusRequestEntityTooLarge)
+				return
+			}
+			c.AbortWithStatus(http.StatusBadRequest)
+			return
+		}
+		c.Next()
+	}
+}
 
 func setupRouter() *gin.Engine {
 
 	r := gin.Default()
+	// Set a lower memory limit for multipart forms (default is 32 MiB)
+	r.MaxMultipartMemory = 8 << 20 // 8 MiB
+	//set a size limit on the uploaded files
+	r.Use(MaxAllowed(9 << 20))
 	r.POST("/uploadFile", handler.UploadFileSingle)
 	r.POST("/uploadFiles", handler.UploadFiles)
 	r.GET("/downloadFile/:folder/:file_name", handler.DownloadFile)
--- router/router_test.go
+++ router/router_test.go
@@ -1,11 +1,19 @@
 package router
 
 import (
+	"bytes"
+	"crypto/rand"
 	"github.com/stretchr/testify/assert"
+	"io"
+	"mime/multipart"
 	"net/http"
 	"net/http/httptest"
+	"os"
+	"path/filepath"
 	"testing"
 )
+
+//todo Extract code to generate files.
 
 func TestSetupRouter(t *testing.T) {
 
@@ -31,6 +39,69 @@ func TestSetupRouter(t *testing.T) {
 			assert.Nil(t, err)
 			assert.Equal(t, tc.expect, w.Code)
 		})
+	}
+
+}
+
+// Test MaxAllowed Middleware 9Mib
+func TestRMMM(t *testing.T) {
+
+	// set fileName and fileSize
+	fileName := "testMMMFile"
+	fileSize := int64(10 * 1024 * 1024) // 10 MB
+
+	// generate len(data) random bytes and writes them into data
+	data := make([]byte, fileSize)
+	_, err := rand.Read(data)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a temporary file to upload
+	file, err := os.CreateTemp("", fileName+"*.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Write data into file
+	if _, err := file.Write(data); err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a new request with the file as the body of the request
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	part, err := writer.CreateFormFile("file", filepath.Base(file.Name()))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if _, err = file.Seek(0, 0); err != nil {
+		t.Fatal(err)
+	}
+	if _, err = io.Copy(part, file); err != nil {
+		t.Fatal(err)
+	}
+	if err = writer.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a test router
+	router := setupRouter()
+	// Create a response recorder to record the response
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/uploadFile", body)
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	router.ServeHTTP(w, req)
+
+	// Check the response status code
+	if w.Code != http.StatusRequestEntityTooLarge {
+		t.Errorf("unexpected status code %d", w.Code)
+		return
+	}
+
+	// Close file
+	if err := file.Close(); err != nil {
+		t.Fatal(err)
 	}
 
 }
