dc8d694d98939c203df87949af99dee7c64ed66d
gallifrey <956465331@qq.com>
2023-04-18 04:48:37 +0800

    feat: add KafkaMiddleware
    - integrate func kafka_message.ProduceWriter() to upload single file
    - integrate func kafka_message.ProduceWriter() to upload multiple files
    - integrate func kafka_message.ProduceWriter() to download file

--- router/router.go
+++ router/router.go
@@ -1,10 +1,115 @@
 package router
 
 import (
+	"encoding/json"
 	"github.com/gallifreyCar/gin-file-server/handler"
+	kafka_message "github.com/gallifreyCar/gin-file-server/kafka-message"
+	log2 "github.com/gallifreyCar/gin-file-server/log"
 	"github.com/gin-gonic/gin"
+	"github.com/segmentio/kafka-go"
+	"log"
 	"net/http"
+	"os"
+	"path"
+	"time"
 )
+
+func KafkaMiddleware(address []string, topic string) gin.HandlerFunc {
+
+	return func(c *gin.Context) {
+
+		//set a logger
+		logFile, logger := log2.InitLogFile("gin-file-server.log", "[KafkaMiddleware]")
+		defer func(file *os.File) {
+			err := file.Close()
+			if err != nil {
+				logger.Println(err)
+			}
+		}(logFile)
+
+		method := c.Request.Method
+		kafkaMessages := make([]kafka.Message, 0)
+		if method == http.MethodGet {
+			folder := c.Param("folder")
+			fileName := c.Param("file_name")
+
+			// Update file download metadata
+			fileInfo := map[string]interface{}{
+				"FilePath":       path.Join("..", "target", "upload", folder),
+				"FileName":       fileName,
+				"DownloadedTime": time.Now().Format(time.DateTime),
+			}
+			// Convert the file info to JSON.
+			message, err := json.Marshal(fileInfo)
+			if err != nil {
+				log.Print(err)
+			}
+			// Construct the Kafka messages.
+			kafkaMessages = []kafka.Message{{
+				Key:   []byte(fileName),
+				Value: message,
+			}}
+
+		}
+		if method == http.MethodPost {
+			//handle multiple files
+			fieldName := c.DefaultPostForm("fieldName", "files")
+			fileCount := len(c.Request.MultipartForm.File)
+
+			//handle single file
+			if fileCount == 1 {
+				fieldName = c.DefaultPostForm("fieldName", "file")
+			}
+
+			logger.Println(fieldName)
+			// Parse the multipart form
+			forms := c.Request.MultipartForm
+			logger.Println(forms)
+			// Get the uploaded files based on the specified field name
+			files := forms.File[fieldName]
+			for _, file := range files {
+				logger.Println(file.Filename)
+				logger.Println(file.Header)
+				//Try to open the file.
+				_, err := file.Open()
+				if err != nil {
+					// If an error occurred, return a bad request response.
+					c.AbortWithError(http.StatusBadRequest, err)
+					return
+				}
+
+				// Create the file info map.
+				fileInfo := map[string]interface{}{
+					"FileName":   file.Filename,
+					"MIMEHeader": file.Header,
+					"FileSize":   file.Size,
+					"UploadTime": time.Now().Format(time.DateTime),
+				}
+				// Convert the file info to JSON.
+				message, err := json.Marshal(fileInfo)
+				if err != nil {
+					log.Print(err)
+				}
+				// Construct the Kafka messages.
+				kafkaMessages = append(kafkaMessages, kafka.Message{
+					Key:   []byte(file.Filename),
+					Value: message,
+				})
+
+			}
+
+		}
+
+		// Write the message to Kafka.
+		err := kafka_message.ProduceWriter(address, topic, kafkaMessages)
+		if err != nil {
+			c.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+		// Continue processing the request.
+		c.Next()
+	}
+}
 
 // MaxAllowed Middleware function to set a size limit on the uploaded files
 func MaxAllowed(n int64) gin.HandlerFunc {
@@ -25,13 +130,17 @@ func MaxAllowed(n int64) gin.HandlerFunc {
 
 func setupRouter() *gin.Engine {
 
+	address1 := os.Getenv("address1")
+	address2 := os.Getenv("address2")
+	address := []string{address1, address2}
+
 	r := gin.Default()
 	// Set a lower memory limit for multipart forms (default is 32 MiB)
 	r.MaxMultipartMemory = 8 << 20 // 8 MiB
 	//set a size limit on the uploaded files
-	r.POST("/uploadFile", MaxAllowed(10<<20), handler.UploadFileSingle)
-	r.POST("/uploadFiles", MaxAllowed(50<<20), handler.UploadFiles)
-	r.GET("/downloadFile/:folder/:file_name", handler.DownloadFile)
+	r.POST("/uploadFile", MaxAllowed(10<<20), handler.UploadFileSingle, KafkaMiddleware(address, "upload-file-single"))
+	r.POST("/uploadFiles", MaxAllowed(50<<20), handler.UploadFiles, KafkaMiddleware(address, "upload-file-multiple"))
+	r.GET("/downloadFile/:folder/:file_name", handler.DownloadFile, KafkaMiddleware(address, "download-file"))
 	r.GET("/uploads/file_name/:file_name", handler.SelectFileLogByName)
 	return r
 }
--- router/router_test.go
+++ router/router_test.go
@@ -3,19 +3,24 @@ package router
 import (
 	"bytes"
 	"crypto/rand"
+	"fmt"
+	"github.com/gallifreyCar/gin-file-server/handler"
+	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 	"io"
 	"mime/multipart"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"os"
+	"path"
 	"path/filepath"
 	"testing"
 )
 
 //todo Extract code to generate files.
 
-func TestSetupRouter(t *testing.T) {
+func TestSetupRouter_old(t *testing.T) {
 
 	testCases := []struct {
 		name        string
@@ -103,6 +108,260 @@ func TestRMMM(t *testing.T) {
 	// Close file
 	if err := file.Close(); err != nil {
 		t.Fatal(err)
+	}
+
+}
+
+func Test_setupRouter(t *testing.T) {
+	tests := []struct {
+		name string
+		want *gin.Engine
+	}{
+		{
+			name: "test uploadFile route",
+			want: func() *gin.Engine {
+				r := gin.Default()
+				r.MaxMultipartMemory = 8 << 20 // 8 MiB
+				r.POST("/uploadFile", MaxAllowed(10<<20), handler.UploadFileSingle, KafkaMiddleware([]string{"address1", "address2"}, "uploadFileSingle"))
+				return r
+			}(),
+		},
+
+		{
+			name: "test uploadFiles route",
+			want: func() *gin.Engine {
+				r := gin.Default()
+				r.MaxMultipartMemory = 8 << 20 // 8 MiB
+				r.POST("/uploadFiles", MaxAllowed(50<<20), handler.UploadFiles, KafkaMiddleware([]string{"address1", "address2"}, "uploadFileMultiple"))
+				return r
+			}(),
+		},
+		{
+			name: "test downloadFile route",
+			want: func() *gin.Engine {
+				r := gin.Default()
+				r.GET("/downloadFile/:folder/:file_name", handler.DownloadFile, KafkaMiddleware([]string{"address1", "address2"}, "downloadFile"))
+				return r
+			}(),
+		},
+		{
+			name: "test selectFileLogByName route",
+			want: func() *gin.Engine {
+				r := gin.Default()
+				r.GET("/uploads/file_name/:file_name", handler.SelectFileLogByName)
+				return r
+			}(),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, setupRouter(), "setupRouter()")
+		})
+	}
+}
+
+func Test_fileUploadSingle(t *testing.T) {
+
+	// Create a temporary file to upload
+	file, err := os.CreateTemp("", "example*.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		err = file.Close()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	// Write some data to the file
+	_, err = file.WriteString("testing file upload")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a new request with the file as the body of the request
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	part, err := writer.CreateFormFile("file", filepath.Base(file.Name()))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a cleanup function to remove the temporary file after the test finishes
+	cleanup := func() {
+		err := os.Remove(file.Name())
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+	if _, err = file.Seek(0, 0); err != nil {
+		cleanup()
+		t.Fatal(err)
+	}
+	if _, err = io.Copy(part, file); err != nil {
+		cleanup()
+		t.Fatal(err)
+	}
+	if err = writer.Close(); err != nil {
+		cleanup()
+		t.Fatal(err)
+	}
+	// Create a test router
+	router := setupRouter()
+	// Create a response recorder to record the response
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/uploadFile", body)
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64)")
+	//Serve the request using the test router
+	router.ServeHTTP(w, req)
+
+	// Check the response status code
+	if w.Code != http.StatusOK {
+		cleanup()
+		t.Errorf("unexpected status code %d", w.Code)
+		return
+	}
+
+}
+
+func Test_fileUploadMultiple(t *testing.T) {
+	fieldName := "testfiles"
+
+	// Create temporary files to upload
+	var files []*os.File
+	for i := 0; i < 3; i++ {
+		tempFiles, err := os.CreateTemp("", "example*.txt")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// Write some data to the file
+		_, err = tempFiles.WriteString(fmt.Sprintf("testing file upload %v", i))
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		files = append(files, tempFiles)
+	}
+
+	//Remove files from os
+	defer func() {
+
+		for _, file := range files {
+			err := file.Close()
+			if err != nil {
+				t.Fatal(err)
+			}
+			err = os.Remove(file.Name())
+			if err != nil {
+				t.Fatal(err)
+			}
+		}
+	}()
+
+	// Create a new request with the files as the body of the request
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	for _, file := range files {
+
+		part, err := writer.CreateFormFile(fieldName, filepath.Base(file.Name()))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if _, err = file.Seek(0, 0); err != nil {
+			t.Fatal(err)
+		}
+		if _, err = io.Copy(part, file); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	if err := writer.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a test router
+	router := setupRouter()
+	// Create a response recorder to record the response
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/uploadFiles", body)
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	req.PostForm = url.Values{"fieldName": {fieldName}}
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64)")
+	// Serve the request using the test router
+	router.ServeHTTP(w, req)
+
+	// Check the response status code
+	if w.Code != http.StatusOK {
+		t.Errorf("unexpected status code %d", w.Code)
+		return
+	}
+
+}
+
+// todo use testCase struct to rewrite this unit test
+func TestDownloadFile(t *testing.T) {
+	folder := "single"
+	fileName := "testfile.txt"
+	folder2 := "multiple"
+	fileName2 := "testfile.txt"
+	downloadAndSave(folder, fileName, t)
+	downloadAndSave(folder2, fileName2, t)
+}
+
+func downloadAndSave(folder, fileName string, t *testing.T) {
+	// Create a test router
+	router := setupRouter()
+	// Create a response recorder to record the response
+	w := httptest.NewRecorder()
+	requestUrl := path.Join("/downloadFile", folder, fileName)
+	req, _ := http.NewRequest("GET", requestUrl, nil)
+
+	// Serve the request using the test router
+	router.ServeHTTP(w, req)
+
+	// Check the response status code
+	if w.Code == http.StatusNotFound {
+		t.Logf("file not found %d", w.Code)
+		return
+	}
+	if w.Code != http.StatusOK {
+		t.Errorf("unexpected status code %d", w.Code)
+		return
+	}
+
+	// Create the download directory if it doesn't exist
+	downloadDirPath := path.Join("..", "target", "download", folder)
+	// Save the files in the local
+	saveFilePath := path.Join(downloadDirPath, fileName)
+
+	err := os.MkdirAll(downloadDirPath, os.ModePerm)
+	if err != nil {
+		t.Errorf("error creating download directory: %v", err)
+		return
+	}
+
+	// Create a new file to save the downloaded content
+	out, err := os.Create(saveFilePath)
+	if err != nil {
+		t.Errorf("error creating file: %v", err)
+		return
+	}
+	defer func() {
+		err = out.Close()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	// Write the downloaded content to the new file
+	_, err = io.Copy(out, w.Body)
+	if err != nil {
+		t.Errorf("error writing file: %v", err)
+		return
 	}
 
 }
