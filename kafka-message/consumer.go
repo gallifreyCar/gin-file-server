bd19ae3bd041bd620b502bc493f45f9f73434434
gallifrey <956465331@qq.com>
2023-04-17 22:48:50 +0800

    feat: add func ConsumeReader() in consumer.go and add unit check

--- kafka-message/consumer.go
+++ kafka-message/consumer.go
@@ -5,19 +5,22 @@ import (
 	"fmt"
 	log2 "github.com/gallifreyCar/gin-file-server/log"
 	"github.com/segmentio/kafka-go"
+	"log"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
 func Consume(topic string) (message string, err error) {
 
 	//set a logger
-	logFile, log := log2.InitLogFile("gin-file-server.log", "[Consume]")
+	logFile, logger := log2.InitLogFile("gin-file-server.log", "[Consume]")
 	defer func(file *os.File) {
 		err := file.Close()
 		if err != nil {
 
-			log.Fatal(err)
+			logger.Fatal(err)
 		}
 	}(logFile)
 	// to consume messages
@@ -26,13 +29,13 @@ func Consume(topic string) (message string, err error) {
 
 	conn, err := kafka.DialLeader(context.Background(), "tcp", address, topic, partition)
 	if err != nil {
-		log.Println("failed to dial leader:", err)
+		logger.Println("failed to dial leader:", err)
 		return "", err
 	}
 
 	err = conn.SetReadDeadline(time.Now().Add(10 * time.Second))
 	if err != nil {
-		log.Println("failed to set ReadDeadline:", err)
+		logger.Println("failed to set ReadDeadline:", err)
 		return "", err
 	}
 	batch := conn.ReadBatch(10e3, 1e6) // fetch 10KB min, 1MB max
@@ -50,12 +53,89 @@ func Consume(topic string) (message string, err error) {
 
 	defer func() {
 		if err := batch.Close(); err != nil {
-			log.Println("failed to close batch:", err)
+			logger.Println("failed to close batch:", err)
 		}
 		if err := conn.Close(); err != nil {
-			log.Println("failed to close connection:", err)
+			logger.Println("failed to close connection:", err)
 		}
 	}()
 	return message, nil
+
+}
+
+func ConsumeReader(brokers []string, topic string, partition int, stop chan bool, done chan int) (err error) {
+
+	//set a logger
+	logFile, logger := log2.InitLogFile("gin-file-server.log", "[ConsumeReader]")
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			logger.Fatal(err)
+		}
+	}(logFile)
+
+	// make a new reader that consumes from topic-A, partition 0, at offset 42
+	r := kafka.NewReader(kafka.ReaderConfig{
+		Brokers:   brokers,
+		Topic:     topic,
+		Partition: partition,
+		MinBytes:  10e3, // 10KB
+		MaxBytes:  10e6, // 10MB
+		MaxWait:   2 * time.Second,
+	})
+	err = r.SetOffset(kafka.LastOffset)
+	if err != nil {
+		logger.Println(err)
+		return err
+	}
+	defer func(r *kafka.Reader) {
+		err := r.Close()
+		if err != nil {
+			logger.Println(err)
+
+		}
+	}(r)
+
+	//create a file "consumerFile" and logger2 to log the topic have been consumed
+	consumerFile, err := os.OpenFile("../target/consumer/"+time.Now().Format(time.DateOnly), os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		logger.Println(err)
+		return err
+	}
+	logger2 := log.New(consumerFile, "[🚄CONSUMER]", log.Ltime|log.Lshortfile)
+	logger2.SetOutput(consumerFile)
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			logger.Println(err)
+		}
+	}(consumerFile)
+
+	//read message from Kafka topic and log it into "consumerFile"
+	logger2.Println("Start consuming...")
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+	for {
+		select {
+		case <-stop:
+			done <- 200
+			logger2.Println("Received stop signal. Stopping the consumer...")
+			return
+
+		case <-signals:
+			logger2.Println("Interrupt signal received. Closing...")
+			return
+
+		default:
+			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+			msg, err := r.ReadMessage(ctx)
+			cancel()
+			if err != nil {
+				logger2.Printf("Error reading message: %v\n", err)
+				continue
+			}
+			logger2.Printf("Received message from partition %d:  %s = %s\n", msg.Partition, string(msg.Key), string(msg.Value))
+		}
+	}
 
 }
--- kafka-message/consumer_test.go
+++ kafka-message/consumer_test.go
@@ -1,8 +1,11 @@
 package kafka_message
 
 import (
+	"context"
+	"os"
 	"strings"
 	"testing"
+	"time"
 )
 
 func TestConsume(t *testing.T) {
@@ -40,6 +43,60 @@ func TestConsume(t *testing.T) {
 			}
 			if !strings.Contains(gotMessage, tt.wantMessage) {
 				t.Errorf("Consume() gotMessage = %v, want %v", gotMessage, tt.wantMessage)
+			}
+		})
+	}
+}
+
+func TestConsumeReader(t *testing.T) {
+
+	_, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	address1 := os.Getenv("address1")
+	address2 := os.Getenv("address2")
+
+	type args struct {
+		brokers   []string
+		topic     string
+		partition int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{name: "test loop receive message", args: args{
+			brokers:   []string{address2, address1},
+			topic:     "test-topic",
+			partition: 0,
+		}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			stop := make(chan bool, 100)
+			done := make(chan int, 100)
+
+			go func() {
+				t.Log("🚩star running")
+				if err := ConsumeReader(tt.args.brokers, tt.args.topic, tt.args.partition, stop, done); (err != nil) != tt.wantErr {
+					t.Errorf("ConsumeReader() error = %v, wantErr %v", err, tt.wantErr)
+				}
+
+			}()
+
+			for {
+				select {
+
+				case <-done:
+					t.Logf("🎉done!!!")
+					close(done)
+					close(stop)
+					return
+				case <-time.After(30 * time.Second):
+					stop <- true
+					t.Logf("🚗sending stop signal...")
+				}
 			}
 		})
 	}
